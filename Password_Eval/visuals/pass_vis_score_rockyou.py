57e77301db315a53372acad02674b961db85861a
rachelHoman <rachelmarie001@aol.com>
2024-03-25 11:46:25 -0700

    updated score count pass length

--- Password_Eval/visuals/pass_vis_score_rockyou.py
+++ Password_Eval/visuals/pass_vis_score_rockyou.py
@@ -9,18 +9,19 @@ with open('filtered_data_score_rockyou.txt', 'r') as file:
     # Read each line
     for line_number, line in enumerate(file, start=1):
         # Split the line to extract password and score
-        parts = line.strip().split(': ', 1)
+        parts = line.split(',')
         # Ensure the line has the expected structure
         if len(parts) == 2:
             # Special handling for passwords with '$' sign
-            if '$' in parts[1]:
-                password = parts[1].replace('$', 'special_character')
+            if '$' in parts[0]:
+                password = parts[0].replace('$', 'special_character')
                 password_length = len(password) + 1  # Adding 1 to length count for '$' sign
                 password = password.replace('special_character', '$')
             else:
-                password = parts[1].strip()
+                password = parts[0].split(':')[1].strip()
                 password_length = len(password)
-            score = int(parts[1].rsplit(', Score:', 1)[1])
+            #score = int(parts[1].rsplit(', Score:', 1)[1])
+            score = int(parts[1].split(':')[1].strip())
             # Append data to lists
             password_lengths.append(password_length)
             scores.append(score)
